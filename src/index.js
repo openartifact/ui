d2c0fd7842029ddd35ff7bc2228a6e63eee13291
Darth Araknus <dartharaknus@gmail.com>
2018-09-04 22:05:29 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { hydrate, render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { Route, Router } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
+import store from './common/store';
+import './index.css';
+import App from './components/App';
+import { unregister } from './common/serviceWorker';
+
+const history = createHistory();
+const rootElement = document.getElementById('root');
+
+const app = (
+  <Provider store={store}>
+    <Router history={history}>
+      <Route component={App} />
+    </Router>
+  </Provider>
+);
+
+if (rootElement.hasChildNodes()) {
+  render(app, rootElement);
+} else {
+  hydrate(app, rootElement);
+}
+
+unregister();
